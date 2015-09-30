2924e68f521f957b85e910b078b243f46b7229cd
Mason Nousain <nousainm@yahoo.com>
2015-09-30 15:18:59 -0400

    Exercise3.5

---
+++ Role.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class Role {
+	public static void main(String[] args) {
+	
+	String input = JOptionPane.showInputDialog("Enter role");
+	
+		switch (input) {
+		case "Administrator":
+		JOptionPane.showMessageDialog(null, "Welcome " + input);
+		break;
+		
+		case "Faculty":
+		JOptionPane.showMessageDialog(null, "Welcome " + input);
+		break;
+		
+		case "Staff":
+		JOptionPane.showMessageDialog(null, "Welcome " + input);
+		break;
+
+		case "Student":
+		JOptionPane.showMessageDialog(null, "Welcome " + input);
+		break;
+
+		case "Guest":
+		JOptionPane.showMessageDialog(null, "Welcome " + input);
+		break; 
+
+		default:
+		JOptionPane.showMessageDialog(null, "Welcome");
+		}
+	}
+	}
+
